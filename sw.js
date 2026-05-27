6e29a278272b127f5758c8b7291bb09e8199894b
hamgiltae <lanlanlu12345@gmail.com>
2026-05-28 07:31:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,11 @@
-const CACHE = 'idcard-v2';
+const CACHE = 'idcard-v10';
 const FILES = [
   '/24/index.html',
-  '/24/manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-  'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&display=swap'
+  '/24/manifest.json'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
@@ -24,13 +20,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      // CDN 리소스는 캐시에 저장
-      if(e.request.url.includes('cdnjs.cloudflare.com') || e.request.url.includes('fonts.googleapis.com')){
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
